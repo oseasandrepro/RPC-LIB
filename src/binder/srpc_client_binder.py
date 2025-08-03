39c46e72e6241766d9cbe31a001b2fc65ac1d457
Oseas Andre <oseasx65@gmail.com>
2025-08-03 13:23:51 -0300

    refactor: enhance logging in RpcClientBinder and improve error handling

--- src/binder/srpc_client_binder.py
+++ src/binder/srpc_client_binder.py
@@ -2,7 +2,7 @@ from util.srpc_serializer import SrpcSerializer
 from interface.srpc_client_binder_interface import SrpcClientBinderInterface
 import socket
 from srpc_exceptions import RpcBinderRequestException
-import os
+import logging
 
 class RpcClientBinder(SrpcClientBinderInterface):
     def __init__(self, host="0.0.0.0"):
@@ -10,7 +10,15 @@ class RpcClientBinder(SrpcClientBinderInterface):
         self.__host = host
         self.__BINDER_PORT = 5000
 
-    def binding_lookup(self):
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(name)s : %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    def binding_lookup(self) -> dict[str, int]:
+        response = None
         try:
             socket_cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             socket_cli.connect((self.__host, self.__BINDER_PORT))
@@ -25,14 +33,28 @@ class RpcClientBinder(SrpcClientBinderInterface):
             if deserialized_response[0] != "200":
                 raise RpcBinderRequestException(deserialized_response[1], code=deserialized_response[0])
                     
-            return deserialized_response[2]
-        except socket.timeout:
-            raise ("Timeout occurred during binding lookup.")
+            response = deserialized_response[2]
+        except ConnectionRefusedError as e:
+            self.logger.error(str(e))
+            self.logger.error("Ensure the RPC Binder Server is running. Mission aborted.")
+            exit(1)
+        except socket.timeout as e:
+            self.logger.error(f"Connection timed out while trying to connect to the RPC Binder Server: {str(e)}")
+            self.logger.error("Ensure the RPC Binder Server is running and reachable. Mission aborted.")
+            exit(1)
         except RpcBinderRequestException as e:
-            print(f"RPC Binder returns an error response during binding lookup: {e}")
-            print("Mission aborted. Exiting.")
-            os._exit(1)
-        except Exception as e:
-            print(f"An error occurred during binding lookup. error: {e}")
-            print("Mission aborted. Exiting.")
-            os._exit(1)
+            self.logger.error(f"RPC Binder Server returns an error response during lookup")
+            self.logger.error(f"RPC Binder Server Error: {str(e)}")
+            self.logger.error("Mission aborted.")
+            exit(1)
+        except socket.error as e:
+            self.logger.error(f"Socket error: {str(e)}")
+            self.logger.error("Mission aborted.")
+            exit(1)
+        except OSError as e:
+            self.logger.error(f"OSError: {str(e)}")
+            self.logger.error("Mission aborted.")
+            exit(1)
+        else:
+            self.logger.info("Binding lookup successful.")
+            return response
