6c70e178dadc79d69bf49574aaa1c0344c71b1b2
Oseas Andre <oseasx65@gmail.com>
2025-08-04 22:07:11 -0300

    refactor: replace print statements with logging for better traceability

--- src/binder/srpc_server_binder.py
+++ src/binder/srpc_server_binder.py
@@ -3,6 +3,7 @@ import threading
 from concurrent.futures import ThreadPoolExecutor
 from util.srpc_serializer import SrpcSerializer
 from interface.srpc_server_binder_interface import SrpcServerBinderInterface
+import logging
 
 class SrpcServerBinder(SrpcServerBinderInterface):
     def __init__(self, host='0.0.0.0'):
@@ -14,6 +15,12 @@ class SrpcServerBinder(SrpcServerBinderInterface):
         self.__shutdown_event = threading.Event()
         self.__binder_socket = None
         self.__set_binder_socket(self.__host, self.__BINDER_PORT)
+
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(name)s : %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S")
 
     def __set_binder_socket(self, host, port):
         self.__binder_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,39 +36,53 @@ class SrpcServerBinder(SrpcServerBinderInterface):
             if request_tuple[0] == "LOOKUP":
                 response_tuple = ("200", "", self.__functions)
                 self.__total_lookup_request += 1
-                print(f"Total lookup requests: {self.__total_lookup_request}")
+                self.logger.info(f"Total lookup requests: {self.__total_lookup_request}")
             elif request_tuple[0] == "REGISTER":
                 req_function = request_tuple[1]
                 port = request_tuple[2]
                 self.__functions[req_function] = port
                 response_tuple = ("200", "", None)
-                print(f"function [{req_function}]  registred in port [{port}]")
+                self.logger.info(f"Function [{req_function}] registered on port [{port}]")
             else:
+                self.logger.error(f"Unknown request type: {request_tuple[0]}")
                 response_tuple = ("500", "erro simulado", None)
 
-                
+        except IOError as e:
+            response_tuple = ("500", str(e), None)
+            self.logger.error(f"IO error: {str(e)}")
+        except OSError as e:
+            response_tuple = ("500", str(e), None)
+            self.logger.error(f"OS error: {str(e)}")
         except Exception as e:
             response_tuple = ("500", str(e), None)
-            print(f"Lookup error: {e}")
+            self.logger.error(f"Error handling lookup request: {str(e)}")
         finally:
             serialized_response = self.__serializer.serialize(response_tuple)
             conn.sendall(serialized_response)
             conn.close()
 
     def start_binder(self):
-        print(f"Binder listening on port {self.__BINDER_PORT}")
+        self.logger.info(f"Binder listening on port {self.__BINDER_PORT}")
         with ThreadPoolExecutor(max_workers=5) as pool:
             while not self.__shutdown_event.is_set():
                 try:
                     client_socket, client_address = self.__binder_socket.accept()
                     pool.submit(self.__handle_lookup_request, client_socket)
+
                 except socket.timeout:
                     continue
-                except OSError:
-                    break
-
+                except OSError as e:
+                    if self.__shutdown_event.is_set():
+                        self.logger.info("Binder shutdown event set, stopping server.")
+                        break
+                    else:
+                        self.logger.error(f"Binder server encountered an error while accepting connections: {str(e)}")
+                        self.logger.error("Mission aborted.")
+                        break
+            exit(1)
+        
     def stop(self):
-        print("Shutting down binder...")
+        self.logger.info("Stopping binder...")
         self.__shutdown_event.set()
         if self.__binder_socket:
             try:
@@ -69,4 +90,4 @@ class SrpcServerBinder(SrpcServerBinderInterface):
             except OSError:
                 pass
             self.__binder_socket.close()
-            print("binder successfully stoped.")
+        self.logger.info("Binder stopped.")
