c40ed18a709343e855e22aad12b6b8c027afd143
Oseas Andre <oseasx65@gmail.com>
2025-10-11 18:38:59 -0300

    refactor: adjust tests, simplify server startup logic

--- tests/integration/run_rpc_server.py
+++ tests/integration/run_rpc_server.py
@@ -1,26 +1,4 @@
-import socket
-import threading
-
 from srpc_calc_server_stub import SrpcCalcServerStub
 
-from binder.srpc_server_binder import SrpcServerBinder
-
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
-
-server_stub = SrpcCalcServerStub(ip_address)
-binder = SrpcServerBinder(ip_address)
-
-# This logic is to gracefully shutdown the server and binder on receiving termination signals
-# TODO: put this logic inside the RpcServerStub and SrpcServerBinder classes
-stop_event = threading.Event()
-try:
-    binder_thread = threading.Thread(target=binder.start_binder, name="binder_thread", daemon=True)
-    binder_thread.start()
-
-    server_stub.start()
-    print("server started, press Ctrl+C to stop\n")
-    stop_event.wait()
-except KeyboardInterrupt:
-    binder.stop()
-    server_stub.stop()
+server_stub = SrpcCalcServerStub()
+server_stub.start()
