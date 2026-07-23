a22170124e7a2442af15d6a971de4cd03cf7cab2
Oseas Andre <oseasx65@gmail.com>
2026-07-22 22:38:02 -0300

    refactor: adjust tests

--- tests/integration/simple_project/client.py
+++ tests/integration/simple_project/client.py
@@ -3,7 +3,7 @@ from srpc_calc_client_stub import SrpcCalcClientStub
 from srpcLib.utils.srpc_network_util import get_lan_ip_or_localhost
 
 SERVER_HOST = get_lan_ip_or_localhost()
-calcStub = SrpcCalcClientStub(SERVER_HOST)
+calcStub = SrpcCalcClientStub(SERVER_HOST, 5000)
 a = 4
 b = 2
 print(f"{calcStub.add(a, b)}")
