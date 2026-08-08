191f04863e428c73301e087850aecd8aec65698c
Oseas Andre <oseasx65@gmail.com>
2026-08-08 00:13:39 -0300

    feat: change the application protocol

--- srpcLib/utils/srpc_network_util.py
+++ srpcLib/utils/srpc_network_util.py
@@ -1,4 +1,97 @@
 import socket
+import struct
+from dataclasses import dataclass
+
+# ############# REQUEST ###############
+# header
+#   protocol_version: 2 byte
+#   id_proc: 2 bytes (máx 65000 procs)
+#   payload_size: 4 bytes
+# payload
+#   payload: n bytes
+HEADER = struct.Struct("!HHI")
+
+PROTOCOL_VERSION = 0
+HEADER_SIZE = HEADER.size
+
+
+@dataclass
+class Request:
+    protocol_version: int
+    proc_id: int
+    payload_size: int
+    payload: bytes
+
+    def serialize(self) -> bytes:
+        header = HEADER.pack(
+            self.protocol_version,
+            self.proc_id,
+            len(self.payload),
+        )
+
+        return header + self.payload
+
+    @classmethod
+    def deserialize_header(cls, data: bytes) -> "Request":
+        if HEADER.size != len(data):
+            raise ValueError("Srpc: Incomplete header")
+
+        protocol_version, proc_id, payload_size = HEADER.unpack_from(data)
+
+        return cls(protocol_version, proc_id, payload_size, None)
+
+
+# ############# RESPONSE ###############
+# header
+#   protocol_version: 2 bytes
+#   type: 2 bytes
+#   payload_size: 4 bytes
+# payload
+#   payload: n bytes
+HEADER = struct.Struct("!HHI")
+
+
+@dataclass
+class Response:
+    protocol_version: int
+    type: int
+    payload_size: int
+    payload: bytes
+
+    def serialize(self) -> bytes:
+        header = HEADER.pack(
+            self.protocol_version,
+            self.type,
+            len(self.payload),
+        )
+
+        return header + self.payload
+
+    @classmethod
+    def deserialize_header(cls, data: bytes) -> "Response":
+        if HEADER.size != len(data):
+            raise ValueError("Srpc: Incomplete header")
+        protocol_version, proc_id, payload_size = HEADER.unpack_from(data)
+
+        return cls(protocol_version, proc_id, payload_size, None)
+
+
+# recive only n bytes , assumes n >= 1
+def recv_n(socket: socket, n: int) -> bytes:
+    bytes = b""
+    len_bytes = 0
+
+    while len_bytes < n:
+        new_bytes = socket.recv(n - len_bytes)
+        if not new_bytes:
+            raise ValueError("Remote host has disconnected, during recv_n execution.")
+
+        bytes += new_bytes
+        len_bytes += len(new_bytes)
+
+    assert len_bytes == n
+
+    return bytes
 
 
 def get_lan_ip_or_localhost():
